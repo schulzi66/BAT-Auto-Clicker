19eae6c1b8cdce69940074cd71ec8e08e4a180f3
Marius Schulze <marius.schulze@t-online.de>
2021-05-16 18:09:13 +0200

    Error Handling

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using Rocketcress.UIAutomation;
 using Rocketcress.UIAutomation.Controls;
-using System;
+using System.Diagnostics;
 using System.Threading;
+using System.Windows.Automation;
 using System.Windows.Forms;
 
 namespace BATClicker
@@ -12,20 +13,29 @@ namespace BATClicker
         {
             while (true)
             {
-                var notification = new UITestControl(By.CPath("/Window/*[@id='PriorityToastView']").AndName("Brave", ByOptions.UseContains));
-                var closeBtn = new UITestControl(By.CPath("/Button[@name='Close']"), notification);
-                var dismissBtn = new UITestControl(By.CPath("/Button[@id='DismissButton']"), notification);
-                if (notification.Exists)
-                {                    
-                    var existingBtn = closeBtn.Exists ? closeBtn : dismissBtn;
-                    var currentCursorPos = Cursor.Position;
-                    existingBtn.MoveMouseToClickablePoint();
+                try
+                {
+                    var notification = new UITestControl(By.CPath("/Window/*[@id='PriorityToastView']").AndName("Brave", ByOptions.UseContains));
+                    if (notification.Exists)
+                    {
+                        var closeBtn = new UITestControl(By.CPath("/Button[@name='Close']"), notification);
+                        var dismissBtn = new UITestControl(By.CPath("/Button[@id='DismissButton']"), notification);
 
-                    existingBtn.Click();
-                    Cursor.Position = currentCursorPos;
-                    Thread.Sleep(500);
+                        var existingBtn = closeBtn.Exists ? closeBtn : dismissBtn;
+                        var currentCursorPos = Cursor.Position;
+                        existingBtn.MoveMouseToClickablePoint();
+
+                        existingBtn.Click();
+                        Cursor.Position = currentCursorPos;
+                        Thread.Sleep(500);
+                    }
+                    Thread.Sleep(100);
                 }
-                Thread.Sleep(100);
+                catch (ElementNotAvailableException ex)
+                {
+                    EventLog.WriteEntry("BATClicker", ex.StackTrace, EventLogEntryType.Error);
+                }
+                
             }
         }
 
